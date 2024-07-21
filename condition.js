3cc24bf6885bdc8066d640a2273645496bfb15ea
shubmodi <shubhammodi4702@gmail.com>
2024-07-21 20:32:56 +0530

    intro js

---
+++ condition.js
@@ -0,0 +1,14 @@
+n =56;
+if(n%2 === 0)
+{
+    a = 'nice'
+    let x ='something';
+    const y = 'something else'
+    console.log("even");
+}else{
+    console.log("odd");
+}
+
+console.log(a);
+// console.log(x);
+console.log(y);
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('hello javascript');
+console.log('nice');
+
+num=7895;
+console.log(num);
+
+myname = 'shubham';
+
+console.log(myname);
+
+console.log(typeof num);
+console.log(typeof myname);
